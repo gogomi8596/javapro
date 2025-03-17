dc1b5d6e11da7ddcdef47d617f42ad466cec5666
gogomi8596 <gogumii8596@gmail.com>
2025-03-17 11:52:21 +0900

    how to use main method

---
+++ src/chapter02/Code01.java
@@ -0,0 +1,12 @@
+package chapter02;
+
+public class Code01 {
+    public static void main(String[] args) {
+        System.out.println("aisw");
+        System.out.print("kim jihyeok\n");
+        System.out.printf("grade 1\n");
+        System.out.println("result:"+(100+100)); //result:200
+        System.out.println(100+200+"won"); //300won
+    }
+}
+
