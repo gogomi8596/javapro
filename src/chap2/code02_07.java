046a60016780777235d72a2d2f7681364ca826f8
gogomi8596 <gogumii8596@gmail.com>
2025-03-24 10:44:01 +0900

    계산기

---
+++ src/chap2/code02_07.java
@@ -0,0 +1,21 @@
+package chap2;
+
+public class code02_07 {
+    public static void main(String[] args) {
+
+        int num1 = 200;
+        int num2 = 100;
+
+        int result = num1 + num2; //덧셈
+        System.out.printf("%d + %d = %d\n", num1, num2, result);
+
+        result = num1 - num2; //뺄셈
+        System.out.printf("%d - %d = %d\n", num1, num2, result);
+
+        result = num1 * num2; //곱셈
+        System.out.printf("%d * %d = %d\n", num1, num2, result);
+
+        result = num1 / num2; //나눗셈
+        System.out.printf("%d / %d = %d\n", num1, num2, result);
+    }
+}
