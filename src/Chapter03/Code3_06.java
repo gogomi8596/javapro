73f8409ebe378e1d61f8dc3d0c71c1eebbfd5684
gogomi8596 <gogumii8596@gmail.com>
2025-04-07 09:49:51 +0900

    산술 연산

--- src/Chapter03/Code3_06.java
+++ src/Chapter03/Code3_06.java
@@ -9,7 +9,7 @@ public class Code3_06 {
         int num1 = s.nextInt();
         System.out.print("enter a number2: ");
         int num2 = s.nextInt();
-
+//제곱
         double result = Math.pow(num1, num2);
 
         System.out.printf("%d의 %d 제곱승은 %.0f입니다!", num1, num2, result);
--- src/Chapter03/Lab.java
+++ src/Chapter03/Lab.java
@@ -12,7 +12,7 @@ public class Lab {
         lb = sc.nextDouble();
         kg = lb * 0.453592;
         System.out.println(kg+"kg");
-
+//산술
         System.out.printf("kilogram: ");
         kg= sc.nextDouble();
         lb = kg * 2.204623;
