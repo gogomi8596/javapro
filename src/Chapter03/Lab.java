cc3c93b19d8a76909929bdd1af563a8b3e65e51a
gogomi8596 <gogumii8596@gmail.com>
2025-04-07 09:49:20 +0900

    산술 연산

---
+++ src/Chapter03/Lab.java
@@ -0,0 +1,21 @@
+package Chapter03;
+
+import java.util.Scanner;
+
+public class Lab {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double lb, kg;
+
+        System.out.printf("libra: ");
+        lb = sc.nextDouble();
+        kg = lb * 0.453592;
+        System.out.println(kg+"kg");
+
+        System.out.printf("kilogram: ");
+        kg= sc.nextDouble();
+        lb = kg * 2.204623;
+        System.out.println(lb+"lb");
+    }
+}
