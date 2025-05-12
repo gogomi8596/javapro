9ea37944a971b47aa9be1cd6e7bbcc952e66b5df
gogomi8596 <gogumii8596@gmail.com>
2025-05-12 10:51:17 +0900

    중첩

---
+++ src/Ft6.java
@@ -0,0 +1,9 @@
+public class Ft6 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.printf("결과: %d, %d\n", i, j);
+            }
+        }
+    }
+}
