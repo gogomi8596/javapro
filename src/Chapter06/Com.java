b9ae48579edb7153f34145a9036d35c57424a874
gogomi8596 <gogumii8596@gmail.com>
2025-05-19 10:44:10 +0900

    com vs my

---
+++ src/Chapter06/Com.java
@@ -0,0 +1,22 @@
+package Chapter06;
+
+import java.util.Scanner;
+
+public class Com {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int com = 0;
+        int my = 0;
+        com = (int)(Math.random()*5 + 1);
+
+        my = sc.nextInt();
+
+        if (com == my) {
+            break;
+        }
+        else {
+            continue;
+        }
+        sc.close();
+    }
+}
