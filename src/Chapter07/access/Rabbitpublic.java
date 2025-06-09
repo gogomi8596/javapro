b7e6ccb4b823bcc7fdd0380a6907eab7a2674099
gogomi8596 <gogumii8596@gmail.com>
2025-06-09 11:21:03 +0900

    토끼3

--- src/Chapter07/access/Rabbitpublic.java
+++ src/Chapter07/access/Rabbitpublic.java
@@ -3,16 +3,4 @@ package Chapter07.access;
 public class Rabbitpublic {
     public String shape;
     public int x, y;
-
-    public void setShape(String shape) {
-        this.shape = shape;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
 }
